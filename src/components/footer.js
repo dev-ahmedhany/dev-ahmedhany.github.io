7ac2e6fc6a39559db542ca98c7e9d3e22c7e0f1e
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-02-26 06:44:48 +0200

    email at phone layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
-import { socialMedia } from '@config';
+import { socialMedia, email } from '@config';
 
 const StyledFooter = styled.footer`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -11,6 +11,27 @@ const StyledFooter = styled.footer`
   min-height: 70px;
   padding: 15px;
   text-align: center;
+`;
+
+const StyledLinkWrapper = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    width: 100%;
+    max-width: 270px;
+    margin: 0 auto 10px;
+    color: var(--light-slate);
+  }
+
+  a {
+    margin: 20px auto;
+    padding: 10px;
+    font-family: var(--font-mono);
+    font-size: var(--fz-xxs);
+    line-height: var(--fz-lg);
+    letter-spacing: 0.1em;
+  }
 `;
 
 const StyledSocialLinks = styled.div`
@@ -42,6 +63,9 @@ const StyledSocialLinks = styled.div`
 
 const Footer = () => (
   <StyledFooter>
+    <StyledLinkWrapper>
+      <a href={`mailto:${email}`}>{email}</a>
+    </StyledLinkWrapper>
     <StyledSocialLinks>
       <ul>
         {socialMedia &&
